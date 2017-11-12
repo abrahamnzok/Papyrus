8ce1370f3fe085b9f0fd8a3009c21deb9de0338b
Unknown <hugo.palla@gmail.com>
2017-11-12 22:56:56 +0100

    getting last character entered and carret position

--- src/Classes/ConcreteInvoker/clientInvoker.java
+++ src/Classes/ConcreteInvoker/clientInvoker.java
@@ -3,14 +3,10 @@ package Classes.ConcreteInvoker;
 import Interfaces.Command.Command;
 import Interfaces.Invoker.Invoker;
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
-
-import java.util.*;
 
 public class clientInvoker extends Application implements Invoker {
 
@@ -34,10 +30,18 @@ public class clientInvoker extends Application implements Invoker {
 
     @Override
     public void start(Stage stage){
+        //application must include start
 
+    }
+
+    @FXML
+    private void initialize() {
+        //Initialize the person table with the two columns.
         //On change listener for the textarea to detect key typed
         this.textarea.textProperty().addListener((observable, oldValue, newValue) -> {
             //Call insert command by taking the next character typed after the caret
+            int carretPosition = textarea.getCaretPosition();
+            System.out.println(newValue.charAt(carretPosition) );
         });
     }
 
