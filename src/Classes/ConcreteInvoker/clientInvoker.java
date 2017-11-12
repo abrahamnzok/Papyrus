82ca7e6e35487ebf8bf6a4b027eac111a2038776
Unknown <hugo.palla@gmail.com>
2017-11-12 21:22:38 +0100

    Adding start method to clientInvoker and a listener on the textarea

--- src/Classes/ConcreteInvoker/clientInvoker.java
+++ src/Classes/ConcreteInvoker/clientInvoker.java
@@ -2,19 +2,43 @@ package Classes.ConcreteInvoker;
 
 import Interfaces.Command.Command;
 import Interfaces.Invoker.Invoker;
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
 
 import java.util.*;
 
-/**
- * 
- */
-public class clientInvoker implements Invoker {
+public class clientInvoker extends Application implements Invoker {
+
+    @FXML
+    private Button copyButton;
+    @FXML
+    private Button cutButton;
+    @FXML
+    private Button pasteButton;
+    @FXML
+    private Button deleteButton;
+    @FXML
+    private TextArea textarea;
 
     private Command command;
     /**
      * Default constructor
      */
     public clientInvoker() {
+    }
+
+    @Override
+    public void start(Stage stage){
+
+        //On change listener for the textarea to detect key typed
+        this.textarea.textProperty().addListener((observable, oldValue, newValue) -> {
+            //Call insert command by taking the next character typed after the caret
+        });
     }
 
     /**
