dc3b40f6c9285725b693b0d06621a9898fb5fbec
Abraham Mubanzo <ab.learn.code@gmail.com>
2017-11-30 16:42:52 +0100

    Changed record method params in the recorder interface.

--- src/interfaces/recorder/Recorder.java
+++ src/interfaces/recorder/Recorder.java
@@ -8,8 +8,9 @@ import interfaces.memento.Memento;
 public interface Recorder {
     /**
      * @param memento  to store
+     * @param myclass the name of the class that created the memento
      */
-    void record(Memento memento) throws NoSuchMethodException;
+    void record(Class myclass, Memento memento) throws NoSuchMethodException;
 
     /**
      *
