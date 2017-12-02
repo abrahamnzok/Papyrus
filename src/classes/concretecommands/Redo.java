f6ef8e03639f65da476b3f331ad43f95ee33c407
Unknown <hugo.palla@gmail.com>
2017-12-02 19:42:01 +0100

    Adding Redo/Undo commands

---
+++ src/classes/concretecommands/Redo.java
@@ -0,0 +1,28 @@
+package classes.concretecommands;
+
+import classes.components.DoUndoEngine;
+import interfaces.command.Command;
+import interfaces.recorder.Recorder;
+
+public class Redo implements Command {
+
+    /**
+     *
+     */
+    private Recorder recorder;
+
+    /**
+     * Calls the engine's method goBackward()
+     */
+    @Override
+    public void execute() throws NoSuchMethodException {
+        ((DoUndoEngine) this.recorder).goForward();
+    }
+
+    /**
+     * @param recorder is the receiver to perform the recording
+     */
+    public void setReceiver(Recorder recorder) {
+        this.recorder = recorder;
+    }
+}
---
+++ src/classes/concretecommands/Undo.java
@@ -0,0 +1,25 @@
+package classes.concretecommands;
+
+import classes.components.DoUndoEngine;
+import interfaces.command.Command;
+import interfaces.recorder.Recorder;
+
+public class Undo implements Command {
+
+    private Recorder recorder;
+
+    /**
+     * Calls the engine's method goForward)
+     */
+    @Override
+    public void execute() throws NoSuchMethodException {
+        ((DoUndoEngine) this.recorder).goBackward();
+    }
+
+    /**
+     * @param recorder is the receiver to perform the recording
+     */
+    public void setReceiver(Recorder recorder) {
+        this.recorder = recorder;
+    }
+}
