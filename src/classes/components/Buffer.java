d2ca2c1cde1c58d0cb7f59358910a7f0f86548e1
Abraham Mubanzo <ab.learn.code@gmail.com>
2017-12-02 14:40:03 +0100

    Javadoc for Buffer is ok.

--- src/classes/components/Buffer.java
+++ src/classes/components/Buffer.java
@@ -1,31 +1,36 @@
 package classes.components;
 
 /**
+ * Buffer object
+ * Its main use is to keep track of the changes made by the commands
+ * Inside a String {@param text} that holds the text
  * 
  */
 public class Buffer implements Cloneable {
 
     /**
-     * 
-     */
-    private String text;
-
-    /**
      * Default constructor
+     * Internal object is an empty {@link String}
      */
     public Buffer() {
         this.text = "";
     }
 
     /**
-     * @param text contained inside the buffer
+     * Object inside which the user's input is stored
+     */
+    private String text;
+
+    /**
+     * @param text the user's input
+     * Which we then store inside our immutable object {@link String} {@param text}
      */
     public void setText(String text) {
         this.text = text;
     }
 
     /**
-     *
+     * @return the content of the buffer if not empty
      */
     public String getText() {
         // TODO implement here
@@ -33,7 +38,7 @@ public class Buffer implements Cloneable {
     }
 
     /**
-     *
+     * @return the length of the buffer. If the buffer is empty, 0 will be returned.
      */
     public int length() {
         // TODO implement here
@@ -41,13 +46,16 @@ public class Buffer implements Cloneable {
     }
 
     /**
-     *
+     * @return true if the buffer is empty, false otherwise.
      */
     public boolean isEmpty() {
         // TODO implement here
         return this.getText().isEmpty();
     }
 
+    /**
+     * @return a shallow copy of this {@link Buffer}
+     */
     @Override
     public Object clone()throws CloneNotSupportedException{
         return super.clone();
