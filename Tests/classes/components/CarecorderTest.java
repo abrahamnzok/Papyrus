9affd8b79b39a7df0acb9ff31750421edbe210cd
Unknown <hugo.palla@gmail.com>
2017-12-03 12:12:36 +0100

    Removing duplicated code

--- Tests/classes/components/CarecorderTest.java
+++ Tests/classes/components/CarecorderTest.java
@@ -432,31 +432,5 @@ public class CarecorderTest {
         assertEquals("We try to delete at different positions and buffer is not empty at the beginning",
                 "idea", this.receiver.getBufferClone().getText());
     }
-
-    @Test
-    public void replayTest9() throws Exception{
-        this.insert.setTextinput("idea");
-        this.insert.setPosition(0);
-        this.startrecording.execute();
-        this.insert.execute();
-        this.delete.setPosition(this.receiver.getBufferClone().length());
-        this.delete.execute();
-        this.delete.setPosition(this.receiver.getBufferClone().length());
-        this.delete.execute();
-        this.delete.setPosition(this.receiver.getBufferClone().length());
-        this.delete.execute();
-        this.delete.setPosition(this.receiver.getBufferClone().length());
-        this.delete.execute();
-        this.stoprecording.execute();
-        this.selection.setStart(0);
-        this.selection.setEnd(this.receiver.getBufferClone().length());
-        this.selection.execute();
-        this.cut.execute();
-        this.insert.setTextinput("idea");
-        this.insert.setPosition(0);
-        this.insert.execute();
-        this.replay.execute();
-        assertEquals("We try to delete at different positions and buffer is not empty at the beginning",
-                "idea", this.receiver.getBufferClone().getText());
-    }
+    
 }
